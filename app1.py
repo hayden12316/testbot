9f5069ffbba4b1d8232615f1e8baf6021ab53b35
Hayden <115068219+hayden12316@users.noreply.github.com>
2022-10-27 15:39:55 +0800

    Add files via upload

--- app1.py
+++ app1.py
@@ -44,7 +44,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    sql_cmd = """select name from users"""
+    sql_cmd = """select * from emp"""
     query_data = db.engine.execute(sql_cmd)
     # print(db.engine.execute(sql_cmd).fetchall())
     # print('#########   ', query_data.fetchone()[0])
